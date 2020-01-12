5cd37a46e87ab0dbe6c706e24f9d3be65beab9b5
Mike <mike.watson@synerzip.com>
2020-01-12 13:14:08 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "mw-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://ho2yvncb6c.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_LT55pFV3x",
+      APP_CLIENT_ID: "6pj1eqtaats5llo4lja7v935re",
+      IDENTITY_POOL_ID: "us-east-2:092b426c-9a95-4666-b28a-7bc293195992"
+    }
+  };
